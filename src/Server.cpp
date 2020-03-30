d4c3d30137b5e1266dc5557a89b8bf26c7f2ad0d
minh-thuc <minhthuc2502@gmail.com>
2020-03-30 16:44:22 +0200

    delete some tests

--- src/Server.cpp
+++ src/Server.cpp
@@ -56,11 +56,11 @@ bool Server::MoveShoulder(int dir){
             #endif
         }
     }
-    //commentaire for test
-    /*if((rc = write(fd_, cmd.c_str(), cmd.size())) == -1){
+
+    if((rc = write(fd_, cmd.c_str(), cmd.size())) == -1){
         LOG_E("write -> %s", strerror(rc));
         return false;
-    }*/
+    }
     return true;
 }
 
@@ -190,48 +190,5 @@ bool Server::MoveWrist(int dir){
 
 ARM Server::getjoint(int index)
 {
-            // for test
-        for (int i=0; i<6; i++) {
-        //set arm's servos limitation
-        switch (i) {
-            case 0:
-                joints_[i].pinNumber = "#0";
-                joints_[i].limitHight = SERVO_0_LIMIT_H;
-                joints_[i].initPosition = SERVO_0_DEFAULT;
-                joints_[i].limitLow = SERVO_0_LIMIT_L;
-                break;
-            case 1:
-                joints_[i].pinNumber = "#1";
-                joints_[i].limitHight = SERVO_1_LIMIT_H;
-                joints_[i].initPosition = SERVO_1_DEFAULT;
-                joints_[i].limitLow = SERVO_1_LIMIT_L;
-                break;
-            case 2:
-                joints_[i].pinNumber = "#2";
-                joints_[i].limitHight = SERVO_2_LIMIT_H;
-                joints_[i].initPosition = SERVO_2_DEFAULT;
-                joints_[i].limitLow = SERVO_2_LIMIT_L;
-                break;
-            case 3:
-                joints_[i].pinNumber = "#3";
-                joints_[i].limitHight = SERVO_3_LIMIT_H;
-                joints_[i].initPosition = SERVO_3_DEFAULT;
-                joints_[i].limitLow = SERVO_3_LIMIT_L;
-                break;
-            case 4:
-                joints_[i].pinNumber = "#4";
-                joints_[i].limitHight = SERVO_4_LIMIT_H;
-                joints_[i].initPosition = SERVO_4_DEFAULT;
-                joints_[i].limitLow = SERVO_4_LIMIT_L;
-                break;
-            case 5:
-                joints_[i].pinNumber = "#5";
-                joints_[i].limitHight = SERVO_5_LIMIT_H;
-                joints_[i].initPosition = SERVO_5_DEFAULT;
-                joints_[i].limitLow = SERVO_5_LIMIT_L;
-                break;
-        }
-    }
-    //
     return joints_[index];
 }
